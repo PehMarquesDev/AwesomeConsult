97c153537f104e694750f37846a040e72df3d135
PehMarquesDev <pehmarquesDev@gmail.com>
2023-05-26 20:20:43 -0300

    Commit inicial do projeto

--- AwesomeConsult-Back/tipo-consulta/index.js
+++ AwesomeConsult-Back/tipo-consulta/index.js
@@ -6,11 +6,11 @@ app.use(bodyParser.json());
 consultas = {};
 
 app.get('/tipoconsultas', (req, res) => {
-consultas = [{"Id":"1","Consulta":"Odontologia"},{"Id":"2","Consulta":"Pediatria"},{"Id":"3","Consulta":"Cardiologia"},{"Id":"4","Consulta":"Nutrição"},{"Id":"5","Consulta":"Neurologia"}];
-res.header("Access-Control-Allow-Origin", "*");
+consultas = [{"id":"1","consulta":"Odontologia"},{"id":"2","consulta":"Pediatria"},{"id":"3","consulta":"Cardiologia"},{"id":"4","consulta":"Nutrição"},{"id":"5","consulta":"Neurologia"}];
+res.header("Access-control-Allow-Origin", "*");
 res.status(201).send(consultas);
 });
 
 app.listen(4000, () => {
-    console.log('Tipos de Consulta ON. Porta 4000');
+    console.log('Tipos de consulta ON. Porta 4000');
 });
